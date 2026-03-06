4d37a0b6bfbeceddabbac87d4193b4fcfae7b910
Faizan Khan <fk790080@gmail.com>
2026-03-06 16:01:00 +0530

    implement authentication routes, middleware setup, and fix bcrypt import

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import genToken from "../config/token.js"
 import User from "../models/user.model.js"
-import bcrypt from "bcrypt.js"
+import bcrypt from "bcryptjs"
 
 export const signUp = async (req, res) => {
     try{
